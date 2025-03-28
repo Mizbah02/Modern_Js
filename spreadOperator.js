0949921da2127e7d119588964c7fa4574e21b6da
Md Mizbah Uddin <111277495+Mizbah02@users.noreply.github.com>
2025-03-28 19:50:08 +0600

    Add files via upload

---
+++ spreadOperator.js
@@ -0,0 +1,16 @@
+
+
+let arr = [1,2,3,4];
+let newArr =[...arr, 5 ,6,7];
+
+let arr1 = [...arr];         //copy of arr , no reference involved .
+
+/*
+   let arr1 = arr;
+   arr.push(10);          // it will also impact arr1
+   console.log(arr1) 
+*/
+
+console.log(newArr);
+
+//we  can also use spread operator for object .
